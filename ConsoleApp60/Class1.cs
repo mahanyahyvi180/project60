d99353af9fef9139064163b3fcda6cf2a5b92ae1
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-08 10:53:06 +0330

    Add files via upload

---
+++ ConsoleApp60/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp60
+{
+    class Class1
+    {
+
+        public class CommissionEmployee : Object
+        {
+            public string FirstName { get; }
+            public string LastName { get; }
+            public string SocialSecurityNumber { get; }
+            private decimal grossSale;
+            private decimal commissionRate;
+
+            public CommissionEmployee(string firstName, string lastName, string socialSecurity, decimal grossSale, decimal commissionRate)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                SocialSecurityNumber = SocialSecurityNumber;
+                GrossSales = grossSale;
+                CommissionRate = commissionRate;
+            }
+
+            public decimal GrossSales
+            {
+                get
+                {
+                    return grossSale;
+                }
+
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GrossSales)}must be>=0");
+                    }
+
+                    grossSale = value;
+                }
+            }
+
+            public decimal CommissionRate
+            {
+                get
+                {
+
+                    return commissionRate;
+                }
+                set
+                {
+                    if (value <= 0 || value >= 1)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(CommissionRate)}must be>0 and<1");
+                    }
+
+                    commissionRate = value;
+                }
+            }
+
+            public decimal Earings() => commissionRate * grossSale;
+
+            public override string ToString() => $"commission employee:{FirstName}{LastName}\n" + $"social security number:{SocialSecurityNumber}\n" + $"gross sales:{grossSale:C}\n" + $"commission rate:{commissionRate:F2}";
+
+            
+                
+            
+
+        }
+    }
+}
---
+++ ConsoleApp60/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp60.Class1;
+
+namespace ConsoleApp60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new CommissionEmployee("Sue", "Jones", "222-22-2222", 10000.00M, 0.6M);
+
+            Console.WriteLine("Employee informaition obtained by properties and method:\n");
+            Console.WriteLine($"First name is{employee.FirstName}");
+            Console.WriteLine($"Last name is{employee.LastName}");
+            Console.WriteLine($"Social security number is{employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales are{employee.GrossSales:C} ");
+            Console.WriteLine($"Commission rate is{employee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are {employee.Earings():C}");
+
+            employee.GrossSales = 5000.0M;
+            employee.CommissionRate = .1M;
+
+            Console.WriteLine("\nUpdated employee information obtained by ToString:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"earnings:{employee.Earings():C}");
+            Console.ReadLine();
+        
+
+
+        }
+    }
+}
